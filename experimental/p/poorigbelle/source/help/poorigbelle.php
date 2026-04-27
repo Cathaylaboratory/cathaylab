ecc38b3750c1b9b1f79886609bb599f617eae16d
LornaSIL <lorna_evans@sil.org>
2026-04-27 13:20:11 -0500

    [poorigbelle] Move to experimental and update files

--- experimental/p/poorigbelle/source/help/poorigbelle.php
+++ experimental/p/poorigbelle/source/help/poorigbelle.php
@@ -16,6 +16,14 @@
     exactly as they are spoken.
 </p>
 
-<p>
-    Users may view the On-Screen keyboard to view the keyboard layout.
-</p>
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
+<p>Due to the size and number of keys, some characters are hidden in the long press. 
+	Press and hold on the key with a little dot on the top right to reveal and use them.</p>
+
+<div id='osk-phone' data-states='default shift numeric'>
+</div>
+
